9142174f90587182fe74028b3faf38c67231f663
Army201 <giraldomorita@gmail.com>
2024-03-11 17:34:56 -0500

    Breeze & Register & Login

--- 04-laravel/larapp/app/Models/User.php
+++ 04-laravel/larapp/app/Models/User.php
@@ -47,7 +47,7 @@ class User extends Authenticatable
      */
     protected $casts = [
         'email_verified_at' => 'datetime',
-        'password' => 'hashed',
+        //'password' => 'hashed',
     ];
 
     //RelationShip: (User has many adoptions)
--- 04-laravel/larapp/routes/web.php
+++ 04-laravel/larapp/routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-use Carbon\Carbon;
 
 /*
 |--------------------------------------------------------------------------
@@ -9,8 +9,8 @@ use Carbon\Carbon;
 |--------------------------------------------------------------------------
 |
 | Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
 |
 */
 
@@ -18,22 +18,14 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/sayhello', function () {
-    return "<h1> Hello TCO 2770672 </h1>";
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-Route::get('/pets/show', function () {
-    $pets = App\Models\Pet::all();
-    //echo var_dump($pets);
-    dd($pets->toArray()); // Dump & Die
-});
-
-Route::get('/pets/view', function () {
-    $pets = App\Models\Pet::all();
-    return view('petsview')->with('pets', $pets);
-});
-
-Route::get('/users/view', function () {
-    $users = App\Models\User::all();
-    return view('usersview')->with('users', $users);
-});
+require __DIR__.'/auth.php';
